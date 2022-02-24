a8c027a347f359d91085b91b898093ed94cda309
Sarmad Ahmad <sarmad9@hotmail.de>
2022-02-24 16:13:39 +0100

    init, add all things

---
+++ main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import Chance from "chance"; 
+
+const chance = new Chance();
+const name = chance.name();
+document.querySelector('#app').innerHTML = `
+<img src="${chance.avatar()}" class="avater"; style=border-color:${chance.color}  />
+  <h1>${name}</h1>
+  <p>${chance.email()}</p>
+  <p>${chance.phone()}</p>
+  <div class="text">
+    <p>Hi, my name is ${name} and I am a ${chance.profession()}. I was born in the year of ${chance.year({max:2022, min:1800})} in the city of ${chance.city()}.
+      My favorite animal is the ${chance.animal()}. But I also love numbers! My favorite number is ${chance.integer({min:0, max:1000})}
+    </p>
+  </div>
+`
